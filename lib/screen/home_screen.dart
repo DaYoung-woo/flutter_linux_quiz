79818e21de6859043d66e39c5723a36c6a18be8f
DaYoung-woo <iamwooda0@gmail.com>
2024-02-05 14:13:39 +0900

    뒤로 가기 기능 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 final homeUrl = Uri.parse('https://jocular-nougat-e60b0a.netlify.app');
@@ -14,19 +15,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double? scrolledUnderElevation;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('리눅스 마스터 기출 문제 풀이'),
-        backgroundColor: const Color.fromARGB(255, 99, 102, 240),
-        scrolledUnderElevation: scrolledUnderElevation,
-        titleTextStyle: const TextStyle(
-          fontSize: 18,
-          fontFamily: 'RobotoMono',
-        ),
-      ),
-      body: WebViewWidget(
-        controller: controller,
-      ),
-    );
+    return WillPopScope(
+        onWillPop: () {
+          var future = controller.canGoBack();
+          future.then((canGoBack) => {
+                if (canGoBack)
+                  {controller.goBack()}
+                else
+                  {SystemNavigator.pop()}
+              });
+          return Future.value(false);
+        },
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('리눅스 마스터 기출 문제 풀이'),
+              backgroundColor: const Color.fromARGB(255, 99, 102, 240),
+              scrolledUnderElevation: scrolledUnderElevation,
+              titleTextStyle: const TextStyle(
+                fontSize: 18,
+                fontFamily: 'RobotoMono',
+              ),
+            ),
+            body: WebViewWidget(
+              controller: controller,
+            ),
+          ),
+        ));
   }
 }
