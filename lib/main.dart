cd33fd15f2ad79a0e1f9b1e79df6b37fbc1b49d1
DaYoung-woo <iamwooda0@gmail.com>
2024-02-07 16:08:11 +0900

    스플래시 지연 시간 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_plandly/screen/home_screen.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-//12
-  runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    ),
-  );
+
+  Future.delayed(const Duration(seconds: 3), () {
+    // 딜레이 후에 네이티브 스플래시 화면을 해제하고 앱을 실행
+    FlutterNativeSplash.remove();
+    runApp(
+      MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+  });
 }
